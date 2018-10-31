94179c4194f54af08fd6f4f24c61030cbd6c95c7
Kendo san <59160969@go.buu.ac.th>
2018-10-31 18:09:05 +0700

    BookDao finish

--- src/Dao/BookDao.java
+++ src/Dao/BookDao.java
@@ -1,4 +1,3 @@
-
 package Dao;
 
 import com.mongodb.MongoClient;
@@ -8,8 +7,14 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.gridfs.GridFSBucket;
 import com.mongodb.client.gridfs.GridFSBuckets;
+import com.mongodb.client.gridfs.model.GridFSUploadOptions;
+import static com.mongodb.client.model.Filters.eq;
 import java.awt.print.Book;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
@@ -17,67 +22,180 @@ import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 public class BookDao {
+
     static MongoClient connectMlab = new MongoClient(new MongoClientURI("mongodb://admin:admin02@ds247223.mlab.com:47223/ebook"));
     static MongoDatabase db = connectMlab.getDatabase("ebook");
     static MongoCollection<Document> Collection;
     static GridFSBucket gridFSBucket = GridFSBuckets.create(db);
-    public static Data.Book getBookById(int eBookId){
+
+    public static Data.Book getBookById(int eBookId) {
         Data.Book result = null;
-          try {
+        try {
             Collection = db.getCollection("Book");
             Document search = new Document("eBookId", eBookId);
-             FindIterable<Document> iterateDoc = Collection.find(search);
+            FindIterable<Document> iterateDoc = Collection.find(search);
             Iterator<Document> iterator = iterateDoc.iterator();
             while (iterator.hasNext()) {
                 Document data = new Document(iterator.next());
                 String eBookName = data.getString("eBookName");
-                String eBookKinds= data.getString("eBookKinds");
-                String publisherName= data.getString("publisherName");
-                String authorName= data.getString("authorName");
-                String fileBook= data.getString("fileBook");
+                String eBookKinds = data.getString("eBookKinds");
+                String publisherName = data.getString("publisherName");
+                String authorName = data.getString("authorName");
+                String fileBook = data.getString("fileBook");
                 int eBookPrice = data.getInteger("eBookPrice");
-                int eBookCoverPrice= data.getInteger("eBookCoverPrice");
+                int eBookCoverPrice = data.getInteger("eBookCoverPrice");
                 int fileEbookSize = data.getInteger("fileEbookSize");
-                int pages= data.getInteger("pages");
-                ObjectId eBookCover_id= data.getObjectId("eBookCover_id");
-                result = new Data.Book(0b1,eBookName,eBookKinds,publisherName,authorName,fileBook,
-                        eBookPrice, eBookCoverPrice,fileEbookSize,pages,eBookCover_id);
+                int pages = data.getInteger("pages");
+                ObjectId eBookCover_id = data.getObjectId("eBookCover_id");
+                result = new Data.Book(0b1, eBookName, eBookKinds, publisherName, authorName, fileBook,
+                        eBookPrice, eBookCoverPrice, fileEbookSize, pages, eBookCover_id);
             }
             return result;
         } catch (Exception eX) {
             eX.printStackTrace();
         }
-           return result;
+        return result;
     }
-    public static boolean getImgeProduc(int eBookId){
-        ObjectId imageBook = null;
-          try {
+
+    public static boolean getImgeProduc(int eBookId) {
+        try {
             Path path = FileSystems.getDefault().getPath("src/img/product.png").toAbsolutePath();
             OutputStream streamToDownloadTo = new FileOutputStream(path.toString());
             Collection = db.getCollection("Book");
-            try {
-            Document searchuser = new Document("eBookId", eBookId);
-            FindIterable<Document> iterateDoc = Collection.find(searchuser);
-            Iterator<Document> iterator = iterateDoc.iterator();
-            while (iterator.hasNext()) {
-                Document data = new Document(iterator.next());
-                if (data.getInteger("eBookId") == eBookId) {
-                   imageBook = data.getObjectId("eBookCover_id");
-                }
-            }
-            } catch (Exception ex) {
-                return false;
-            }
-            gridFSBucket.downloadToStream(imageBook, streamToDownloadTo);
+            Data.Book book = getBookById(eBookId);
+            gridFSBucket.downloadToStream(book.eBookCover_id, streamToDownloadTo);
             streamToDownloadTo.close();
             System.out.println("Download to File Success");
             return true;
         } catch (Exception ex) {
-            System.err.println("Download to File Failure = "+ex);
+            System.err.println("Download to File Failure = " + ex);
         }
         return false;
+    }
+
+    public static boolean deleteBook(int eBookId) {
+        Data.Book book = getBookById(eBookId);
+        Collection = db.getCollection("Book");
+        try {
+            gridFSBucket.delete(book.eBookCover_id);
+            Collection.deleteOne(eq("eBookId", book.eBookId));
+            System.out.println("Delete Success");
+            return true;
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return false;
+    }
+
+    public static ObjectId uploadImageToChunk(File file, ObjectId eBookId) {
+        Collection = db.getCollection("Book");
+        try {
+            InputStream streamToUploadFrom = new FileInputStream(file);
+            GridFSUploadOptions options = new GridFSUploadOptions()
+                    .chunkSizeBytes(1024)
+                    .metadata(new Document("eBookId", eBookId));
+
+            ObjectId key = gridFSBucket.uploadFromStream("eBookImage", streamToUploadFrom, options);
+            streamToUploadFrom.close();
+            System.out.println("Upload Success");
+            return key;
+        } catch (IOException ex) {
+            System.err.println("Upload Failure" + ex);
+        }
+        return null;
+    }
+
+    public static boolean insertBook(Data.Book book, File filePic) {
+        Collection = db.getCollection("Book");
+        ObjectId keyBookId;
+        try {
+            Document doc = new Document();
+            doc.put("eBookId", getMaxId());
+            doc.put("eBookName", book.eBookName);
+            doc.put("eBookKinds", book.eBookKinds);
+            doc.put("publisherName", book.publisherName);
+            doc.put("authorName", book.authorName);
+            doc.put("eBookPrice", book.eBookPrice);
+            doc.put("eBookCoverPrice", book.eBookCoverPrice);
+            doc.put("fileBookSize", book.fileBookSize);
+            doc.put("pages", book.pages);
+            doc.put("eBookCover_id", "");
+            Collection.insertOne(doc);
+            keyBookId = (ObjectId) doc.get("_id");
+            ObjectId keyBookImage = uploadImageToChunk(filePic, keyBookId);
+            Collection.updateOne(eq("eBookName", book.eBookName), new Document("$set", new Document("eBookCover_id", keyBookImage)));
+            return true;
+        } catch (Exception ex) {
+            System.err.println("insert Failure");
+        }
+        return false;
+    }
+
+    public static boolean updateBook(Data.Book book, File filePic) {
+        ObjectId keyBookId = null;
+        try {
+            Collection = db.getCollection("Book");
+            Document found = (Document) Collection.find(new Document("eBookId", book.eBookId)).first();
+            if (found != null) {
+                Document document = new Document();
+                document.put("eBookId", book.eBookId);
+                document.put("eBookName", book.eBookName);
+                document.put("eBookKinds", book.eBookKinds);
+                document.put("publisherName", book.publisherName);
+                document.put("authorName", book.authorName);
+                document.put("eBookPrice", book.eBookPrice);
+                document.put("eBookCoverPrice", book.eBookCoverPrice);
+                document.put("fileBookSize", book.fileBookSize);
+                document.put("pages", book.pages);
+                if (filePic == null) {
+                    Bson bsonUpdate = document;
+                    Bson updateOpearation = new Document("$set", bsonUpdate);
+                    Collection.updateOne(found, updateOpearation);
+                } else {
+                    try {
+                        Document search = new Document("eBookId", book.eBookId);
+                        FindIterable<Document> iterateDoc = Collection.find(search);
+                        Iterator<Document> iterator = iterateDoc.iterator();
+                        while (iterator.hasNext()) {
+                            Document data = new Document(iterator.next());
+                            keyBookId = data.getObjectId("_id");
+                            gridFSBucket.delete(data.getObjectId("eBookCover_id"));
+                        }
+                    } catch (Exception exp) {
+                    }
+                    ObjectId keyBookImage = uploadImageToChunk(filePic, keyBookId);
+                    document.put("eBookCover_id", keyBookImage);
+                     Bson bsonUpdate = document;
+                    Bson updateOpearation = new Document("$set", bsonUpdate);
+                    Collection.updateOne(found, updateOpearation);
+                }
+            }
+        } catch (Exception exp) {
+            exp.printStackTrace();
+        }
+        return false;
+    }
+
+    public static int getMaxId() {
+        int Max = Integer.MIN_VALUE;
+        try {
+            Collection = db.getCollection("Book");
+            FindIterable<Document> iterateDoc = Collection.find();
+            Iterator<Document> iterator = iterateDoc.iterator();
+            while (iterator.hasNext()) {
+                Document data = new Document(iterator.next());
+                if (data.getInteger("eBookId") > Max) {
+                    Max = data.getInteger("eBookId");
+                }
+            }
+            return Max + 1;
+        } catch (Exception eX) {
+            eX.printStackTrace();
+            return 0;
+        }
     }
 }
--- src/Data/Book.java
+++ src/Data/Book.java
@@ -6,17 +6,17 @@ import org.bson.types.ObjectId;
 
 public class Book {
 
-    int eBookId;
-    String eBookName;
-    String eBookKinds;
-    String publisherName;
-    String authorName;
-    String fileBook;
-    int eBookPrice;
-    int eBookCoverPrice;
-    int fileEbookSize;
-    int pages;
-    ObjectId eBookCover_id;
+    public int eBookId;
+    public String eBookName;
+    public String eBookKinds;
+    public String publisherName;
+    public String authorName;
+    public String fileBook;
+    public int eBookPrice;
+    public int eBookCoverPrice;
+    public int fileBookSize;
+    public int pages;
+    public ObjectId eBookCover_id;
     public Book(int pages){
         this.pages = pages;
     }
@@ -30,7 +30,7 @@ public class Book {
         this.fileBook = fileBook;
         this.eBookPrice = eBookPrice;
         this.eBookCoverPrice = eBookCoverPrice;
-        this.fileEbookSize = fileEbookSize;
+        this.fileBookSize = fileEbookSize;
         this.pages = pages;
         this.eBookCover_id = eBookCover_id;
     }
